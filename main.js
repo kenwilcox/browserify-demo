6072103c53260f4448d3158671c158635cf10391
Ken Wilcox <wilcoxk@gmail.com>
2014-12-22 10:42:52 -0700

    added more unique items

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 var unique = require('uniq');
-var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+var data = [1, 2, 3, 4, 5, 5, 5, 6, 1, 3, 7, 6, 2, 4, 3, 3, 4, 5, 2, 1, 1];
 console.log("raw data: " + data);
 console.log("unique data: " + unique(data));
